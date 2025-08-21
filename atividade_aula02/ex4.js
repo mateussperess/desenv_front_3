51f7daab4da133f6472b17689bc3b87a6f397cf9
mateussperess <mateuspereslopesl@outlook.com>
2025-08-21 19:50:33 -0300

    Atividade aula 2 - Exercicios

--- atividade_aula02/ex4.js
+++ atividade_aula02/ex4.js
@@ -1,75 +1,108 @@
-// Exercício 4 
-// Crie um formulário html que será utilizado para cadastrar empregados de uma empresa. 
-// Este formulário será composto por um campo input nome, um campo input salario e um 
-// campo select cargo, o qual possui os valores estagiário, desenvolvedor, analista, 
-// administrador e gerente, além de um botão cadastrar. 
-// Faça com que o clique no botão cadastrar verifique os campos acima citados, validando 
-// os mesmos de forma a garantir que o nome não seja vazio, o salário positivo e o cargo, 
-// um dos valores selecionados.  
-// A partir desses campos, caso todos tenham sido preenchidos corretamente, crie um 
-// objeto que armazena estes atributos do funcionário, armazenando o mesmo em um 
+// Exercício 4
+// Crie um formulário html que será utilizado para cadastrar empregados de uma empresa.
+// Este formulário será composto por um campo input nome, um campo input salario e um
+// campo select cargo, o qual possui os valores estagiário, desenvolvedor, analista,
+// administrador e gerente, além de um botão cadastrar.
+// Faça com que o clique no botão cadastrar verifique os campos acima citados, validando
+// os mesmos de forma a garantir que o nome não seja vazio, o salário positivo e o cargo,
+// um dos valores selecionados.
+// A partir desses campos, caso todos tenham sido preenchidos corretamente, crie um
+// objeto que armazena estes atributos do funcionário, armazenando o mesmo em um
 // array de funcionários.
 
-let nomeFuncionario = document.getElementById('nome');
-let salarioFuncionario = document.getElementById('salario');
-let cargoFuncionario = document.getElementById('cargo');
+let nomeFuncionario = document.getElementById("nome");
+let salarioFuncionario = document.getElementById("salario");
+let cargoFuncionario = document.getElementById("cargo");
 
-let cargos = ['estagiario', 'desenvolvedor', 'analista', 'administrador', 'gerente']
+let cargos = [
+  "estagiario",
+  "desenvolvedor",
+  "analista",
+  "administrador",
+  "gerente",
+];
 const funcionarios = [];
 
-const response = document.getElementById('response')
+const response = document.getElementById("response");
+const table = document.getElementById("table")
 
-const form = document.getElementById('form')
-form.addEventListener('submit', (e) => {
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let nome = nomeFuncionario.value
-  let salario = salarioFuncionario.value
-  let cargo = cargoFuncionario.value
+  let nome = nomeFuncionario.value;
+  let salario = salarioFuncionario.value;
+  let cargo = cargoFuncionario.value;
 
-  let flatCargoValido = false;
+  let dadosOK = false;
+  let erros = [];
+  let flagCargoValido = false;
 
-  response.innerHTML = ''
-  let mensagemErro = document.createElement('h3')
-  mensagemErro.style.color = 'red'
-  
-  if(nome == '' || nome == null)
-  {
-    mensagemErro.innerHTML = 'O nome para cadastrar está inválido!'
-    response.appendChild(mensagemErro)
+  response.innerHTML = "";
+  let mensagemErro = document.createElement("h3");
+  mensagemErro.style.color = "red";
+
+  if (nome === "" || nome === null) {
+    erros.push("nome");
   }
 
-  if(salario <= 0)
-  {
-    mensagemErro.innerHTML = 'O salário deve ser um valor positivo!'
-    response.appendChild(mensagemErro)
+  if (salario === "" || salario === null || salario <= 0) {
+    erros.push("salario");
   }
 
-  cargos.forEach(cargoValido => {
-    if(cargo == cargoValido)
-    {
-      flatCargoValido = true
+  cargos.forEach((cargoValido) => {
+    if (cargo == cargoValido) {
+      flagCargoValido = true;
     }
   });
 
-  if(!flatCargoValido)
-  {
-    mensagemErro.innerHTML = 'O cargo não está correto!'
-    response.appendChild(mensagemErro)
+  if (!flagCargoValido) {
+    erros.push("cargo");
   }
 
-  let funcionario = {
-    nome: nome,
-    salario: salario,
-    cargo: cargo
+  if (erros.length <= 0) {
+    dadosOK = true;
   }
 
-  console.log(funcionario)
+  let erroH3 = document.createElement("h3");
+  erroH3.style.color = "red";
+  erros.forEach((erro) => {
+    erroH3.innerHTML = `Não foi possível cadastrar o funcionario pois o campo ${erro} não está de acordo com os critérios de cadastro`;
+  });
 
-  let mensagemSucesso = document.createElement('h3')
-  mensagemSucesso.style.color = 'green'
-  mensagemSucesso.innerHTML = 'Funcionário cadastrado com sucesso!'
-  response.appendChild(mensagemSucesso)
+  response.appendChild(erroH3);
 
-  funcionarios.push(funcionario);
-})
+  if (dadosOK) {
+    let funcionario = {
+      nome: nome,
+      salario: salario,
+      cargo: cargo,
+    };
 
+    let mensagemSucesso = document.createElement("h3");
+    mensagemSucesso.style.color = "green";
+    mensagemSucesso.innerHTML = "Funcionário cadastrado com sucesso!";
+    response.appendChild(mensagemSucesso);
+
+    popularListaFuncionarios(funcionario);
+
+    funcionarios.push(funcionario);
+  }
+});
+
+function popularListaFuncionarios(funcionario)
+{
+  let tr = document.createElement("tr")
+  let tdNome = document.createElement("td");
+  let tdSalario = document.createElement("td");
+  let tdCargo = document.createElement("td");
+
+  tdNome.textContent = funcionario.nome
+  tdSalario.textContent = funcionario.salario
+  tdCargo.textContent = funcionario.cargo
+
+  tr.appendChild(tdNome)
+  tr.appendChild(tdSalario)
+  tr.appendChild(tdCargo)
+
+  table.appendChild(tr)
+}
